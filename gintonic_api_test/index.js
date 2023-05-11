ace49e31e09c289e6af99387bdf198892f5c6887
MarcoGuglielmino <marco.guglielmino@musement.com>
2023-05-11 11:40:44 +0200

    feat: try a solution for CORS

--- gintonic_api_test/index.js
+++ gintonic_api_test/index.js
@@ -22,19 +22,33 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Server Started!"));
 
+const setHeadersMiddleware = (req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "*");
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  // Pass to next layer of middleware
+  next();
+};
+
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello from HomePage!"));
+app.get("/", (_, res) => res.send("Hello from HomePage!"));
 
-app.use("/drinks", drinkRoutes);
+app.use("/drinks", setHeadersMiddleware, drinkRoutes);
 
-app.use("/category", categoryRoutes);
+app.use("/category", setHeadersMiddleware, categoryRoutes);
 
-app.use("/search", searchRoutes);
+app.use("/search", setHeadersMiddleware, searchRoutes);
 
-app.get("/popular", getTopDrinks);
+app.get("/popular", setHeadersMiddleware, getTopDrinks);
 
-app.get("/latest", getLatestDrinks);
+app.get("/latest", setHeadersMiddleware, getLatestDrinks);
 
 const PORT = process.env.PORT;
 
